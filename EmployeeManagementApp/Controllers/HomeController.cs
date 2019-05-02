8b87c00f22eda4c2ef9102505616f8e306a41d40
shawonahmed2020 <shawonahmed.net@gmail.com>
2019-05-02 07:03:08 +0600

    validation added

--- EmployeeManagementApp/Controllers/HomeController.cs
+++ EmployeeManagementApp/Controllers/HomeController.cs
@@ -40,10 +40,14 @@ namespace EmployeeManagementApp.Controllers
             return View();
         }
         [HttpPost]
-        public RedirectToActionResult Create(Employee employee)
+        public IActionResult Create(Employee employee)
         {
-            Employee newEmployee = _employeeRepository.Add(employee);
-            return RedirectToAction("details", new { id = newEmployee.Id });
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("details", new {id = newEmployee.Id});
+            }
+            return View();
         }
     }
 }
--- EmployeeManagementApp/Models/Employee.cs
+++ EmployeeManagementApp/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace EmployeeManagementApp.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$", ErrorMessage = "Email is Invalid")]
         public string Email { get; set; }
         public Dept Department { get; set; }
 
